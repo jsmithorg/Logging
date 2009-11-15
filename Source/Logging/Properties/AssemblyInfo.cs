566dc2cb2e934e98bfd2ced46ba2988c22150810
Justin Smith <justin@jsmith.org>
2009-11-15 11:58:17 -0800

    [initial commit]

---
+++ Source/Logging/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Logging Framework")]
+[assembly: AssemblyDescription("Logging framework for .net WPF and Silverlight projects.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JSmith.org")]
+[assembly: AssemblyProduct("Logging Framework")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bba3ff4d-57d7-4612-a2d6-7425c3c4bf71")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+//[assembly: AssemblyVersion("0.1.1103.2")]
+//[assembly: AssemblyFileVersion("0.1.1103.2")]
---
+++ Source/Unit Tests/Logging.Tests/LoggingTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Windows.Input;
+
+namespace MediaBrowser.Tests
+{
+    [TestFixture]
+    public class LoggingTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+
+        }//end method
+
+    }//end class
+
+}//end namespace
